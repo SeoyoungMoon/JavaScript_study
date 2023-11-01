89c89abaf2226740cb3cda88523a64889f350782
SeoyoungMoon <anstd9@naver.com>
2023-11-01 18:02:40 +0900

    array함수, reduce() 심화공부

--- Basic/13_mustKnow_array_function.js
+++ Basic/13_mustKnow_array_function.js
@@ -81,3 +81,102 @@ let iveMembers3 = [
     iveMembers,     // 리스트의 0번째 인덱스값이 리스트인거. 
 ];
 console.log(iveMembers3);
+
+console.log('--------------------------------------');
+
+let iveMembers4 = iveMembers;
+
+console.log(iveMembers4);
+console.log(iveMembers4 === iveMembers);        //이건 true야
+
+console.log([                                   //이건 false야
+    ...iveMembers,
+] === iveMembers);
+
+// join()
+console.log(iveMembers.join());
+console.log(iveMembers.join('/'));
+console.log(iveMembers.join(', '));
+
+// sort() : 오름차순
+iveMembers.sort();
+console.log(iveMembers);
+
+iveMembers.reverse();
+console.log(iveMembers);
+
+let numbers = [
+    1,
+    9,
+    7,
+    5,
+    3,
+]
+console.log(numbers);
+
+console.log("_____________________________________________________");
+/**
+ * a, b를 비교했을 때 
+ * 1. a를 b보다 나중에 정렬하려면 (뒤에두려면) 0보다 큰 숫자를 반환.
+ * 2. a를 b보다 먼저 정렬하려면 (앞에두려면) 0보다 작은 숫자를 반환.
+ * 3. 원래 순서를 그대로 두려면 0을 반환. 
+*/
+
+numbers.sort((a, b)=>{
+    return a > b ? 1 : 0;
+})
+console.log(numbers);
+
+numbers.sort((a, b)=>{      //내림차순
+    return a > b ? -1 : 1;
+})
+console.log(numbers);
+
+numbers.sort((a, b)=>{      //오름차순
+    return a > b ? 1 : -1;
+})
+console.log(numbers);
+
+console.log('--------------------------------------');
+
+// map()
+console.log(iveMembers.map((x) => x));
+console.log(iveMembers.map((x) => `아이브: ${x}`));     //이거 백틱이야!
+
+console.log(iveMembers.map((x) => {
+    if(x ===  '안유진'){
+        return `아이브: ${x}`;      //안유진일때에만 앞에 아이브를 붙이고
+    } else {
+        return x;                   //나머지는 그대로 리턴. 
+    }
+}));
+
+console.log(iveMembers);            //근데 해보면 iveMembers는 그대로 나와. 원래의 array는 건드리지 않고 새로운 array를 반환. 
+
+//filter()  : map이랑 똑같아. 모든값을 순회하면서 x에 값을 넣어줘. //해당되는 모든 값을 찾아. 
+numbers = [1, 8, 7, 6, 3];
+
+console.log(numbers.filter((x) => true));
+console.log(numbers.filter((x) => x % 2 === 0));        //해당되는 모든 값을 찾아. 
+
+//find()    : 처음부터 찾다가 해당되는 첫번째 값만 반환.
+console.log(numbers.find((x) => x % 2 === 0));
+
+//findIndex()    : 값의 인덱스를 반환.
+console.log(numbers.findIndex((x) => x % 2 === 0));     //해당하는 값인 8은 인덱스의 1번째 자리에 있어서 1을 반환.
+
+//reduce()      --> 어려워 잘 이해하고 넘어가자! 
+/**
+ * 여기서 p+n은 콜백함수이고, 0은 초기값이야. 
+ * 순서가 p -> n -> p+n -> p -> n -> p+n -> p -> n -> p+n -> p -> n -> p+n ... 반복을 해
+ * 1. 초기값인 0이 p에 입력된다. 
+ * 2. numbers 어레이의 첫번째 값인 1이 n에 입력된다. 
+ * 3. p+n 즉, 0+1의 결과값인 1이 반환된다. 
+ * 4. 반환한 값 1이 p에 입력된다. 
+ * 5. 어레이의 두번째 값인 8이 n에 입력된다. 
+ * 6. p+n 즉, 1+8의 결과값인 9가 반환된다. 
+ * 7. 반환한 값 9가 p에 입력된다. 
+ * 8. numbers 리스트의 모든 값들을 다 순회할 때까지 반복. 
+ * 9. 결국 모든 값을 다 더한 25가 반환된다. 
+ */
+console.log(numbers.reduce((p, n) => p + n, 0));    
